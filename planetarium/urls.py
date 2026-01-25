a8b8fb06cd726065211d4f21516b196306b55e82
toomuchtearz <toomuchtearz@gmail.com>
2026-01-26 00:18:31 +0200

    add orders url router

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from planetarium.views import ThemeViewSet, ShowViewSet, PlanetariumDomeViewSet, SessionViewSet
+from planetarium.views import ThemeViewSet, ShowViewSet, PlanetariumDomeViewSet, SessionViewSet, OrderViewSet
 
 router = DefaultRouter()
 router.register("themes", ThemeViewSet,)
 router.register("shows", ShowViewSet,)
 router.register("domes", PlanetariumDomeViewSet,)
 router.register("sessions", SessionViewSet,)
+router.register("orders", OrderViewSet,)
 
 urlpatterns = [
     path("", include(router.urls))
