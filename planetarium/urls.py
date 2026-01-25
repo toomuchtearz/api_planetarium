be68f9a72d01d5749e4bb7506634f1321650acb7
toomuchtearz <toomuchtearz@gmail.com>
2026-01-25 19:10:13 +0200

    add url router for Session

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from planetarium.views import ThemeViewSet, ShowViewSet, PlanetariumDomeViewSet
+from planetarium.views import ThemeViewSet, ShowViewSet, PlanetariumDomeViewSet, SessionViewSet
 
 router = DefaultRouter()
 router.register("themes", ThemeViewSet,)
 router.register("shows", ShowViewSet,)
 router.register("domes", PlanetariumDomeViewSet,)
+router.register("sessions", SessionViewSet,)
 
 urlpatterns = [
     path("", include(router.urls))
