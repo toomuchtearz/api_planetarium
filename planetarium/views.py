6058a072f1ebc24c06392db9f3b21a0014cdd45c
toomuchtearz <toomuchtearz@gmail.com>
2026-01-25 18:06:08 +0200

    add viewsets for Theme & Show

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from planetarium.models import Theme, Show
+from planetarium.serializers import ThemeSerializer, ShowSerializer, ShowListSerializer
+
+
+class ThemeViewSet(viewsets.ModelViewSet):
+    queryset = Theme.objects.all()
+    serializer_class = ThemeSerializer
+
+
+class ShowViewSet(viewsets.ModelViewSet):
+    queryset = Show.objects.all()
+    serializer_class = ShowSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ShowListSerializer
+        return self.serializer_class
