05b115da8bfcb797c7099ace88258bb81a42ad0b
toomuchtearz <toomuchtearz@gmail.com>
2026-01-25 17:42:55 +0200

    add planetarium app & rest_framework to INSTALLED_APPS

--- api_planetarium/settings.py
+++ api_planetarium/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "planetarium",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
