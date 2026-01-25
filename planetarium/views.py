d58067112a47a7b91d5ab78461246774a8d41666
toomuchtearz <toomuchtearz@gmail.com>
2026-01-25 18:19:16 +0200

    update ShowViewSet

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 
 from planetarium.models import Theme, Show
-from planetarium.serializers import ThemeSerializer, ShowSerializer, ShowListSerializer
+from planetarium.serializers import ThemeSerializer, ShowSerializer, ShowListSerializer, ShowRetrieveSerializer
 
 
 class ThemeViewSet(viewsets.ModelViewSet):
@@ -16,4 +16,14 @@ class ShowViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action == "list":
             return ShowListSerializer
+        elif self.action == "retrieve":
+            return ShowRetrieveSerializer
         return self.serializer_class
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action in ("list", "retrieve"):
+            queryset = queryset.prefetch_related(
+                "themes",
+            )
+        return queryset
