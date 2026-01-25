da057676c5802c4bc4f3c66c8bce12c02ffb99d4
toomuchtearz <toomuchtearz@gmail.com>
2026-01-25 19:09:04 +0200

    add serializers for Session

--- planetarium/serializers.py
+++ planetarium/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from planetarium.models import Theme, Show, PlanetariumDome
+from planetarium.models import Theme, Show, PlanetariumDome, Session
 
 
 class ThemeSerializer(serializers.ModelSerializer):
@@ -57,4 +57,31 @@ class PlanetariumDomeSerializer(serializers.ModelSerializer):
             "name",
             "rows",
             "seats_in_row",
+        )
+
+
+class SessionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Session
+        fields = (
+            "id",
+            "show",
+            "dome",
+            "show_time"
+        )
+
+class SessionListSerializer(serializers.ModelSerializer):
+    show_title = serializers.StringRelatedField(source="show")
+    dome_name = serializers.StringRelatedField(source="dome")
+    dome_capacity = serializers.IntegerField(source="dome.capacity")
+
+    class Meta:
+        model = Session
+        fields = (
+            "id",
+            "show_title",
+            "dome_name",
+            "dome_capacity",
+            "show_time"
         )
