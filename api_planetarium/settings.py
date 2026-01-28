46ee1b94b7a8081efff7140b0042174fbb9973d8
toomuchtearz <toomuchtearz@gmail.com>
2026-01-29 01:05:44 +0200

    add custom IsAdminOrIfAuthenticatedReadOnly permission

--- api_planetarium/settings.py
+++ api_planetarium/settings.py
@@ -133,6 +133,9 @@ AUTH_USER_MODEL = "user.User"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "planetarium.permissions.IsAdminOrIfAuthenticatedReadOnly",
     )
 }
 
