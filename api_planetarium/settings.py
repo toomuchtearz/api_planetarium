31a7634e94cc6fce87085fd7c58fe577c27da7b8
toomuchtearz <toomuchtearz@gmail.com>
2026-01-29 01:44:20 +0200

    add throttling

--- api_planetarium/settings.py
+++ api_planetarium/settings.py
@@ -137,6 +137,14 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": (
         "planetarium.permissions.IsAdminOrIfAuthenticatedReadOnly",
     ),
+    "DEFAULT_THROTTLE_CLASSES": (
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ),
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "100/day",
+        "user": "1000/day",
+    }
 }
 
 SIMPLE_JWT = {
