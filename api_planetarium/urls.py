3f3b42c41943917febde7406204e20304ffc8153
toomuchtearz <toomuchtearz@gmail.com>
2026-01-29 00:21:47 +0200

    add static urls

--- api_planetarium/urls.py
+++ api_planetarium/urls.py
@@ -1,7 +1,10 @@
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from api_planetarium import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,4 +12,4 @@ urlpatterns = [
     path("api/user/", include("user.urls", namespace="user")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-] + debug_toolbar_urls()
+] + debug_toolbar_urls() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
