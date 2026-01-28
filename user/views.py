cd436ed3f58f73fe85dfc4682f05b205d4237bfb
toomuchtearz <toomuchtearz@gmail.com>
2026-01-28 16:53:18 +0200

    add user serializer and manage + create views

--- user/views.py
+++ user/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    def get_object(self):
+        return self.request.user
+
+    serializer_class = UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
