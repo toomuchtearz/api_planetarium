21f8950cf7326e197fc1316fa993cad1b3801fed
toomuchtearz <toomuchtearz@gmail.com>
2026-01-28 16:40:27 +0200

    moved JWT urls to global urls

--- api_planetarium/urls.py
+++ api_planetarium/urls.py
@@ -1,10 +1,12 @@
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/planetarium/", include("planetarium.urls", namespace="planetarium")),
     path("api/user/", include("user.urls", namespace="user")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ] + debug_toolbar_urls()
--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from user.views import ManageUserView, CreateUserView
 
 app_name = "user"
 
-
 urlpatterns = [
-   path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-   path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+   path("register/", CreateUserView.as_view(), name="create"),
+   path("me/", ManageUserView.as_view(), name="manage"),
 ]
