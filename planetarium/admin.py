e28e5677130bcbf585efaaa0a2060320fac4994a
toomuchtearz <toomuchtearz@gmail.com>
2026-01-31 04:23:02 +0200

    register models on admin panel

--- planetarium/admin.py
+++ planetarium/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from planetarium.models import (
+    Theme,
+    Show,
+    PlanetariumDome,
+    Session,
+    Order,
+    Ticket
+
+)
+
+
+@admin.register(Theme)
+class ThemeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Show)
+class ShowAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PlanetariumDome)
+class PlanetariumDomeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Session)
+class SessionDomeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    pass
